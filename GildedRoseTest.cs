c8bd25aa2d8eff326fe5ab4f3336edd9f4bfb633
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-03-27 08:56:14 +0000

    Initial documented test case

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -9,9 +9,12 @@ namespace csharp
         [Test]
         public void foo()
         {
+            //arrange
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
+            //act
             app.UpdateQuality();
+            //assert
             Assert.AreEqual("fixme", Items[0].Name);
         }
     }
