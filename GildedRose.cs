acff8c752c7011a95ea3290a9d29b86ca1d25d7c
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-04-02 14:22:26 +0100

    refactor: extracting out aged brie

--- GildedRose.cs
+++ GildedRose.cs
@@ -12,19 +12,86 @@ namespace csharp
             this.Items = Items;
         }
 
+       
+        
         public void UpdateQuality()
         {
             foreach (var item in Items)
             {
-              
-                if (!item.IsItem(ValidItems.AgedBrie) && !item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
+                if (item.IsItem(ValidItems.AgedBrie))
                 {
-                    if (item.Quality > 0)
+                    UpdateAgedBrieItemQuanity(item);
+                }
+                else
+                {
+                    UpdateItemQuanity(item);
+                }
+                
+            }
+        }
+        private static void UpdateAgedBrieItemQuanity(Item item)
+        {
+            if (!item.IsItem(ValidItems.AgedBrie) && !item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
+            {
+                if (item.Quality > 0)
+                {
+                    if (!item.IsItem(ValidItems.SulfurasHandOfRagnaros))
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+
+
+                    if (item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
+                    {
+                        if (item.SellIn < 11)
                         {
-                            item.Quality = item.Quality - 1;
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
                         }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            if (!item.IsItem(ValidItems.SulfurasHandOfRagnaros))
+            {
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0)
+            {
+                if (!item.IsItem(ValidItems.AgedBrie))
+                {
+                    if (!item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (!item.IsItem(ValidItems.SulfurasHandOfRagnaros))
+                            {
+                                item.Quality = item.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        item.Quality = item.Quality - item.Quality;
                     }
                 }
                 else
@@ -32,60 +99,82 @@ namespace csharp
                     if (item.Quality < 50)
                     {
                         item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+        }
+
+
+        private static void UpdateItemQuanity(Item item)
+        {
+            if (!item.IsItem(ValidItems.AgedBrie) && !item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
+            {
+                if (item.Quality > 0)
+                {
+                    if (!item.IsItem(ValidItems.SulfurasHandOfRagnaros))
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
 
                
-                        if (item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
+                    if (item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
+                    {
+                        if (item.SellIn < 11)
                         {
-                            if (item.SellIn < 11)
+                            if (item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
+                        }
 
-                            if (item.SellIn < 6)
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }
                 }
+            }
 
                
-                if (!item.IsItem(ValidItems.SulfurasHandOfRagnaros))
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
+            if (!item.IsItem(ValidItems.SulfurasHandOfRagnaros))
+            {
+                item.SellIn = item.SellIn - 1;
+            }
 
-                if (item.SellIn < 0)
+            if (item.SellIn < 0)
+            {
+                if (!item.IsItem(ValidItems.AgedBrie))
                 {
-                    if (!item.IsItem(ValidItems.AgedBrie))
+                    if (!item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
                     {
-                        if (!item.IsItem(ValidItems.BackstagePassToTalk80ETCConcert))
+                        if (item.Quality > 0)
                         {
-                            if (item.Quality > 0)
+                            if(!item.IsItem(ValidItems.SulfurasHandOfRagnaros))
                             {
-                                if(!item.IsItem(ValidItems.SulfurasHandOfRagnaros))
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
+                                item.Quality = item.Quality - 1;
                             }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
                         }
                     }
                     else
                     {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
