feb1aaa7392dd9bdd64793560517eca4eba6bc9b
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-04-02 12:54:36 +0100

    test: moved approval tests into something that will let me test multiple cases

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -1,20 +1,54 @@
-﻿using csharp;
+﻿using System;
+using System.IO;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using csharp;
 
 namespace GildedRoseCSharp
 {
+    [UseReporter(typeof(DiffReporter))]
     [TestFixture]
     public class GildedRoseTest
     {
-        [TestCase("foo")]
-        public void foo(string name)
+
+        //generate a bunch of test cases
+        public IList<Item> GenerateTestItems(string[] names, int[] sellIns, int[] qualities)
         {
-            //arrange
-            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            //act
-            app.UpdateQuality();
-            //assert
-            Assert.AreEqual("fixme", Items[0].Name);
+            IList<Item> Items = new List<Item>
+            {
+            };
+            foreach (var name in names)
+            {
+                foreach (var sellIn in sellIns)
+                {
+                    foreach (var quality in qualities)
+                    {
+                        var item = new Item{ Name =name, Quality = quality, SellIn = sellIn};
+                        Items.Add( item);
+                    }
+
+                }
+
+            }
+
+            return Items;
+
         }
+
+         [Test]
+         public void Test2()
+         {
+
+
+             //arrange
+             var Items = GenerateTestItems( new [] { "Aged Brie" }, new []{1, 5,10,2,4}, new[] { 1, 2 });
+             GildedRose app = new GildedRose(Items);
+             //act
+             app.UpdateQuality();
+             //assert
+             Approvals.VerifyAll(Items,"All Items");
+
+         }
     }
 }
